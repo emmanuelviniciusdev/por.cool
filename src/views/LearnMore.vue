9c290d5e459b39bc5dd1d9918c439a666ee3c215
emmanuelviniciusdev <emanuel_vns@hotmail.com>
2020-01-28 10:44:43 -0300

    Últimas tasks a serem feitas antes de lançar o beta [1], [2]

--- src/views/LearnMore.vue
+++ src/views/LearnMore.vue
@@ -3,15 +3,16 @@
     <div class="columns is-multiline">
         <div class="column">
             <h1 class="title">sobre o porcool</h1>
-            <p>antes de mais nada, meu nome é emmanuel e fui eu que dei vida a esta coisa estranha que você está vendo aqui. prazer.</p>
-            <p>continuando, o porcool veio de uma necessidade minha de transformar uma planilha excel, que eu mesmo utilizava para fazer um pequeno controle dos meus gastos pessoais, para alguma coisa mais prática de ser utilizada porque, convenhamos, ficar editando planilha é MUITO CHATO, e eu já não aguentava mais. e foi aí que eu pensei: "hmmm, acho que não seria lá uma má ideia se eu juntasse os meus superpoderes de programador com a minha preguiça e ódio em editar planilhas para desenvolver alguma coisa que faça isso de maneira totalmente automática para mim".</p>
-            <br>
-            <p>e foi daí que surgiu o porcool. :)</p>
-            <br>
-            <p>eu sei que isto aqui é uma coisa simples, e pode parecer patético para algumas pessoas.</p>
-            <p><i>mas eu desenvolvi isto daqui PARA MIM utilizar, tá ok? se não gostou, caia fora.</i></p>
-            <br>
-            <p>brincadeiras à parte, seja muito bem-vindo de qualquer jeito...</p>
+            <p class="is-italic has-text-weight-bold">História...</p>
+            <p><i>Antes de continuar, deixa eu me apresentar: meu nome é Emmanuel e fui eu que dei vida ao porcool.</i></p>
+            <p>O porcool surgiu de uma necessidade de transformar uma simples planilha de controle financeiro pessoal para alguma coisa mais "elaborada", que fizesse algumas coisas de maneira automatizada para mim.</p>
+            <p>Editar planilhas é ruim, chato, horrível. Você pode até automatizá-las com suas funções matemáticas mirabolantes e deixá-las uns 2% mais usual. Mas elas continuarão sendo planilhas.</p>
+            <p>Então eu pensei: <i>eu poderia criar alguma coisa simples, que faça as coisas de maneira mais fácil e prática pro meu dia a dia...</i></p>
+            <p>No começo, a minha ideia era desenvolver alguma coisa só para mim utilizar, mas no meio do caminho me veio na cabeça que poderiam existir mais pessoas que também abonimassem planilhas, então eu desenvolvi a ferramenta de uma forma que tornasse possível sua utilização também por outras pessoas.</p>
+            <p class="is-italic has-text-weight-bold">O que vai dar pra mim fazer com o porcool?</p>
+            <p>Bom, por enquanto, o porcool é uma ferramenta muito simples e, enquanto estiver em sua versão beta, você conseguirá gerenciar os seus gastos do dia a dia e a sua renda mensal. <i>Tudo o que uma simples planilha faria, só que melhor.</i></p>
+            <p class="is-italic has-text-weight-bold">Tudo na nuvem</p>
+            <p>Como o porcool é um aplicativo <i>web</i>, todos os seus dados ficarão disponíveis na nuvem e acessíveis de qualquer dispositivo que possua acessso à internet.</p>
             <br>
             <router-link :to="{name: 'signup'}">criar uma conta</router-link><br>
             <router-link :to="{name: 'signin'}">entrar</router-link>
