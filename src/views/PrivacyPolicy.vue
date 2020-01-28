6e28840321201a97ea0e395be751967884559b62
emmanuelviniciusdev <emanuel_vns@hotmail.com>
2020-01-28 17:30:21 -0300

    Últimas tasks a serem feitas antes de lançar o beta [5]

---
+++ src/views/PrivacyPolicy.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1 class="title">Política de Privacidade</h1>
+    <p>O Porcool respeita o direito à privacidade e zela pela segurança dos dados de seus usuários.</p>
+    <p>Deixamos claro que poderemos modificar ou alterar a Política de Privacidade a qualquer momento e, cabe a você, analisar periodicamente a mais atualizada versão desta Política de Privacidade.</p>
+
+    <h2 class="subtitle has-text-weight-bold">Informações coletadas</h2>
+    <p>Em nosso servidor, coletaremos informações cadastrais (nome, sobrenome, e-mail, senha) e informações financeiras (gastos, saldos).</p>
+    <p>Utilizamos o Google Analytics. Com ele, poderá também ser coletados, mas com a possibilidade de não ser, os seguintes dados:</p>
+    <div class="content">
+      <ul>
+        <li>número de usuários e sessões</li>
+        <li>duração da sessão</li>
+        <li>sistemas operacionais</li>
+        <li>modelos de dispositivos</li>
+        <li>geografia</li>
+        <li>primeiras inicializações</li>
+        <li>aberturas do aplicativo</li>
+        <li>atualizações do aplicativo</li>
+        <li>compras no app</li>
+      </ul>
+    </div>
+    <p>Ao utilizar o Porcool, você concorda e consente com tais coletas de dados.</p>
+
+    <h2 class="subtitle has-text-weight-bold">Uso das informações coletadas</h2>
+    <p>Os dados coletados ficarão disponíveis e serão utilizados apenas dentro de nossos servidores. Terceiros não terão acesso a eles, exceto mediante a algum pedido de justiça.</p>
+
+    <h2 class="subtitle has-text-weight-bold">A respeito de cookies</h2>
+    <p>O Porcool utiliza-se de cookies para manter ativa a sessão do usuário autenticado.</p>
+
+    <h2 class="subtitle has-text-weight-bold">Segurança</h2>
+    <p>O Porcool se utiliza de medidas de segurança comercialmente razoáveis para proteger as informações contidas em nosso banco de dados. Contudo, sabe-se que nenhum sistema de segurança é 100% seguro. Sendo assim, por mais que façamos o possível para proteger suas informações pessoais, não temos como garantir a absoluta segurança em nossa plataforma. É possível que os dados que você forneça em nossos sistemas possam ser interceptados por terceiros durante a transmissão. Portanto, é uma decisão sua a utilização de nossos serviços nestas condições.</p>
+    <p>Sua senha é secreta, então a mantenha em segurança e não a divulgue para ninguém.</p>
+    <p>Não solicitaremos, em hipótese alguma, sua senha ou qualquer outra informação pessoal por e-mail, telefone ou por qualquer outro lugar senão o Porcool.</p>
+    <p>Caso compartilhe o seu dispositivo com alguém ou vai utilizar o Porcool através de um dispositivo público, recomendamos que sempre se desconecte da sua conta e feche a janela do seu navegador assim que terminar de utilizar o Porcool. É uma boa prática e uma boa medida de segurança.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrivacyPolicy"
+};
+</script>
+
+<style lang="scss" scoped>
+h2 {
+  margin: 0 !important;
+  margin-top: 20px !important;
+}
+</style>
